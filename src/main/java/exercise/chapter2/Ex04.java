ae8fdb4db686c195d6fb00889455c1155780781e
najs <najs1225@gmail.com>
2025-06-17 17:17:10 +0900

    flatMap 예제 추가

--- src/main/java/exercise/chapter2/Ex04.java
+++ src/main/java/exercise/chapter2/Ex04.java
@@ -4,6 +4,8 @@ import reactor.core.publisher.Flux;
 
 public class Ex04 {
     public static void main(String[] args) {
+        //flatMap 예제 : 문장을 단어로 변환, 순서를 보장하지 않음
+        //concatMap 의 경우 순서를 보장함
         Flux<String> sentences = Flux.just("Hello World", "Reactive Programming");
 
         sentences.flatMap(sentence -> {
